a41e448d70c4fa4bb0fd5f98dec610b8f93bdf1b
James-Michael-3301 <jamariego02@gmail.com>
2026-02-16 23:08:24 +0800

    Update footer.php
    Added footer

--- footer.php
+++ footer.php
@@ -1,2 +1,20 @@
-</body>
-</html>
+<footer>
+    <div class="footer-container">
+
+        <div class="footer-nav">
+            <a href="discover.php">Discover</a>
+            <a href="map.php">Find A Spot</a>
+            <a href="about.php">About</a>
+        </div>
+
+        <div class="footer-logo">
+            <img src="images/NookFinder Footer.png" alt="Nook Finder Logo">
+        </div>
+
+    </div>
+
+    <div class="footer-bottom">
+        Copyright &copy; NookFinder. All rights are reserved.
+    </div> 
+
+</footer>
